531df1f5b67b0499047000431712fa70a06f1d46
Simon de Villiers <simon.devilliers@octoenergy.com>
2023-11-10 21:47:36 +0000

    Make into WASM app

--- src/main.rs
+++ src/main.rs
@@ -1,406 +1,157 @@
-// use std::collections::HashMap;
-use indexmap::IndexMap;
-use std::error::Error;
-use std::fs::File;
-use std::io;
-use std::path::Path;
+use std::collections::HashMap;
 
-use clap::Parser;
-use csv::Reader;
+use gloo::file::callbacks::FileReader;
+use gloo::file::File;
+use web_sys::{DragEvent, Event, FileList, HtmlInputElement};
+use yew::html::TargetCast;
+use yew::{html, Callback, Component, Context, Html};
+mod mfp;
 
-/// Processes raw MFP app export CSV to chris' spreadsheet format
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// path of the CSV file to process
-    #[arg(short, long)]
-    path: String,
+// TODO: use https://plippe.github.io/blog/2021/07/12/rust-wasm-github.html for github pages
 
-    /// type of data to process
-    #[arg(short, long)]
-    data_type: String,
+#[allow(dead_code)]
+struct FileDetails {
+    name: String,
+    file_type: String,
+    data: String,
 }
 
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
-struct RawNutritionRecord {
-    date: String,
-    meal: String,
-    calories: String,
-    fat: String,
-    saturated_fat: String,
-    polyunsaturated_fat: String,
-    monounsaturated_fat: String,
-    trans_fat: String,
-    cholesterol: String,
-    sodium: String,
-    potassium: String,
-    carbohydrates: String,
-    fiber: String,
-    sugar: String,
-    protein: String,
-    vitamin_a: String,
-    vitamin_c: String,
-    calcium: String,
-    iron: String,
-    note: Option<String>,
+pub enum Msg {
+    Loaded(String, String, String),
+    Files(Vec<File>),
 }
 
-#[derive(Debug, serde::Serialize)]
-struct ProcessedNutritionRecord {
-    protein: f64,
-    carbohydrates: f64,
-    fat: f64,
+pub struct App {
+    readers: HashMap<String, FileReader>,
+    files: Vec<FileDetails>,
 }
 
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
-struct WeightRecord {
-    date: String,
-    body_fat: String,
-    weight: String,
-}
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
 
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
-struct StepsRecord {
-    date: String,
-    exercise: String,
-    _type: String,
-    exercise_calories: String,
-    exercise_minutes: String,
-    sets: String,
-    rps: String,
-    kilograms: String,
-    steps: String,
-    note: String,
-}
-
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
-struct RawWorkoutRecord {
-    date: String,
-    workout_name: String,
-    duration: String,
-    exercise_name: String,
-    set_order: String,
-    weight: String,
-    reps: String,
-    distance: String,
-    seconds: String,
-    notes: String,
-    workout_notes: String,
-    rpe: String,
-}
-
-#[derive(Debug, serde::Serialize)]
-struct ProcessedWorkoutRecord {
-    workout_name: String,
-    weight: String,
-    reps: String,
-}
-
-#[derive(Debug, Clone)]
-struct ExerciseRow {
-    sets: Vec<(String, String)>,
-}
-
-#[derive(Debug, Clone)]
-struct ExercisesMap {
-    map: IndexMap<String, ExerciseRow>,
-}
-
-impl ExercisesMap {
-    fn new(exercise_name: String, exercises: ExerciseRow) -> Self {
-        let mut map = IndexMap::new();
-        map.insert(exercise_name, exercises);
-        Self { map }
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            readers: HashMap::default(),
+            files: Vec::default(),
+        }
     }
-}
 
-#[derive(Debug, Clone)]
-struct ExerciseRowsForADate {
-    exercises: ExercisesMap,
-}
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Loaded(file_name, file_type, data) => {
+                self.files.push(FileDetails {
+                    data,
+                    file_type,
+                    name: file_name.clone(),
+                });
+                self.readers.remove(&file_name);
+                true
+            }
+            Msg::Files(files) => {
+                for file in files.into_iter() {
+                    let file_name = file.name();
+                    let file_type = file.raw_mime_type();
 
-impl ExerciseRowsForADate {
-    fn new(exercises: ExercisesMap) -> Self {
-        Self { exercises }
-    }
-}
+                    println!("{}", &file_name);
+                    let task = {
+                        let link = ctx.link().clone();
+                        let file_name = file_name.clone();
 
-#[derive(Debug)]
-struct LastFourDaysWorkouts {
-    data: IndexMap<String, ExerciseRowsForADate>,
-}
-
-impl From<Vec<RawWorkoutRecord>> for LastFourDaysWorkouts {
-    fn from(raw_workout_records: Vec<RawWorkoutRecord>) -> Self {
-        // {
-        //  date: {
-        //      exercise_1: [(weight, reps),(weight, reps)],
-        //      exercise_2: [(weight, reps),(weight, reps)],
-        //  }
-        // }
-        let mut records_grouped_by_date: IndexMap<String, ExerciseRowsForADate> = IndexMap::new();
-
-        for mut raw_workout_record in raw_workout_records {
-            raw_workout_record.date = raw_workout_record
-                .date
-                .split_whitespace()
-                .next()
-                .unwrap()
-                .to_string();
-
-            let set_tuple_copy = (
-                raw_workout_record.weight.clone(),
-                raw_workout_record.reps.clone(),
-            );
-            let set_tuple_copy_2 = (
-                raw_workout_record.weight.clone(),
-                raw_workout_record.reps.clone(),
-            );
-            let name_copy: String = raw_workout_record.exercise_name.clone();
-
-            records_grouped_by_date
-                .entry(raw_workout_record.date)
-                // date exists; find exercise
-                .and_modify(|erd| {
-                    erd.exercises
-                        .map
-                        .entry(raw_workout_record.exercise_name)
-                        // exercise exists: append to row
-                        .and_modify(|er| {
-                            er.sets
-                                .push((raw_workout_record.weight, raw_workout_record.reps));
+                        gloo::file::callbacks::read_as_text(&file, move |res| {
+                            let res_ok = res.expect("failed to read file");
+                            web_sys::console::log_1(&res_ok.clone().into());
+                            let file_bytes = res_ok.as_bytes();
+                            let process_file_type = {
+                                if file_name.contains("Exercise") {
+                                    "steps"
+                                } else if file_name.contains("Nutrition") {
+                                    "nutrition"
+                                } else if file_name.contains("Measurement") {
+                                    "weight"
+                                } else {
+                                    ""
+                                }
+                            };
+                            web_sys::console::log_1(&process_file_type.into());
+                            let processed_file = mfp::process(file_bytes, process_file_type);
+                            web_sys::console::log_1(&processed_file.clone().into());
+                            link.send_message(Msg::Loaded(file_name, file_type, processed_file))
                         })
-                        // new exercise: create row with one tuple
-                        .or_insert(ExerciseRow {
-                            sets: vec![set_tuple_copy],
-                        });
-                })
-                // date doesn't exist; create it with a new exercise map
-                .or_insert(ExerciseRowsForADate::new(ExercisesMap::new(
-                    name_copy,
-                    ExerciseRow {
-                        sets: vec![set_tuple_copy_2],
-                    },
-                )));
+                    };
+                    self.readers.insert(file_name, task);
+                }
+                true
+            }
         }
+    }
 
-        // Only want to get the most recent four days to paste into the spreadsheet
-        records_grouped_by_date.reverse();
-        records_grouped_by_date.truncate(4);
-
-        LastFourDaysWorkouts {
-            data: records_grouped_by_date,
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div id="wrapper">
+                <p id="title">{ "Process your MyFitnessPal data for Chris' Google Sheets" }</p>
+                <label for="file-process">
+                    <div
+                        id="drop-container"
+                        ondrop={ctx.link().callback(|event: DragEvent| {
+                            event.prevent_default();
+                            let files = event.data_transfer().unwrap().files();
+                            Self::process_files(files)
+                        })}
+                        ondragover={Callback::from(|event: DragEvent| {
+                            event.prevent_default();
+                        })}
+                        ondragenter={Callback::from(|event: DragEvent| {
+                            event.prevent_default();
+                        })}
+                    >
+                        <i class="fa fa-cloud-upload"></i>
+                        <p>{"Drag and drop your files here, or click to choose files"}</p>
+                    </div>
+                </label>
+                <input
+                    id="file-process"
+                    type="file"
+                    accept=".csv"
+                    multiple={true}
+                    onchange={ctx.link().callback(move |e: Event| {
+                        let input: HtmlInputElement = e.target_unchecked_into();
+                        Self::process_files(input.files())
+                    })}
+                />
+                <div id="preview-area">
+                    { for self.files.iter().map(Self::view_file) }
+                </div>
+            </div>
         }
     }
 }
 
-fn read_csv<P: AsRef<Path>>(filename: P) -> Result<Reader<File>, Box<dyn Error>> {
-    let file = File::open(filename)?;
-    Ok(csv::Reader::from_reader(file))
-}
-
-fn deserialize_nutrition_csv<R>(
-    mut rdr: csv::Reader<R>,
-) -> Result<Vec<RawNutritionRecord>, Box<dyn Error>>
-where
-    R: std::io::Read,
-{
-    let new_headers = rdr
-        .headers()
-        .iter()
-        .next()
-        .unwrap()
-        .iter()
-        .map(|h| match h {
-            "Fat (g)" => "fat",
-            "Sodium (mg)" => "sodium",
-            "Carbohydrates (g)" => "carbohydrates",
-            "Protein (g)" => "protein",
-            "Saturated Fat" => "saturated_fat",
-            "Polyunsaturated Fat" => "polyunsaturated_fat",
-            "Monounsaturated Fat" => "monounsaturated_fat",
-            "Trans Fat" => "trans_fat",
-            "Vitamin A" => "vitamin_a",
-            "Vitamin C" => "vitamin_c",
-            _ => h,
-        })
-        .map(|h| h.to_lowercase())
-        .collect::<csv::StringRecord>();
-
-    rdr.set_headers(new_headers);
-
-    // Partition suggestion taken from https://doc.rust-lang.org/rust-by-example/error/iter_result.html
-    let (records, _): (Vec<_>, Vec<_>) = rdr.deserialize().partition(Result::is_ok);
-    let records: Vec<RawNutritionRecord> = records.into_iter().map(Result::unwrap).collect();
-
-    Ok(records)
-}
-
-fn deserialize_weight_csv<R>(mut rdr: csv::Reader<R>) -> Result<Vec<WeightRecord>, Box<dyn Error>>
-where
-    R: std::io::Read,
-{
-    let new_headers = rdr
-        .headers()
-        .iter()
-        .next()
-        .unwrap()
-        .iter()
-        .map(|h| match h {
-            "Body Fat %" => "body_fat",
-            _ => h,
-        })
-        .map(|h| h.to_lowercase())
-        .collect::<csv::StringRecord>();
-
-    rdr.set_headers(new_headers);
-
-    // Partition suggestion taken from https://doc.rust-lang.org/rust-by-example/error/iter_result.html
-    let (records, _): (Vec<_>, Vec<_>) = rdr.deserialize().partition(Result::is_ok);
-    let records: Vec<WeightRecord> = records.into_iter().map(Result::unwrap).collect();
-
-    Ok(records)
-}
-
-fn build_nutrition_csv_for_clipboard(records: Vec<RawNutritionRecord>) {
-    let mut wtr = csv::Writer::from_writer(io::stdout());
-    let mut records_grouped_by_date: IndexMap<String, ProcessedNutritionRecord> = IndexMap::new();
-
-    for record in records {
-        records_grouped_by_date
-            .entry(record.date)
-            .and_modify(|pr| {
-                pr.protein += record.protein.parse::<f64>().unwrap();
-                pr.carbohydrates += record.carbohydrates.parse::<f64>().unwrap();
-                pr.fat += record.fat.parse::<f64>().unwrap();
-            })
-            .or_insert(ProcessedNutritionRecord {
-                protein: record.protein.parse::<f64>().unwrap(),
-                carbohydrates: record.carbohydrates.parse::<f64>().unwrap(),
-                fat: record.fat.parse::<f64>().unwrap(),
-            });
-    }
-    let processed_records = records_grouped_by_date.values().collect::<Vec<_>>();
-
-    for record in processed_records {
-        wtr.serialize(record).ok();
-    }
-    wtr.into_inner().ok();
-}
-
-fn generic_build_csv_for_clipboard<R>(records: Vec<R>)
-where
-    R: serde::Serialize,
-{
-    let mut wtr = csv::Writer::from_writer(io::stdout());
-
-    for record in records {
-        wtr.serialize(record).ok();
-    }
-    wtr.into_inner().ok();
-}
-
-fn deserialize_workout_csv(mut rdr: Reader<File>) -> Result<Vec<RawWorkoutRecord>, Box<dyn Error>> {
-    let new_headers = rdr
-        .headers()
-        .iter()
-        .next()
-        .unwrap()
-        .iter()
-        .map(|h| match h {
-            "Workout Name" => "workout_name",
-            "Exercise Name" => "exercise_name",
-            "Set Order" => "set_order",
-            "Workout Notes" => "workout_notes",
-            _ => h,
-        })
-        .map(|h| h.to_lowercase())
-        .collect::<csv::StringRecord>();
-
-    rdr.set_headers(new_headers);
-
-    // Partition suggestion taken from https://doc.rust-lang.org/rust-by-example/error/iter_result.html
-    let (records, _): (Vec<_>, Vec<_>) = rdr.deserialize().partition(Result::is_ok);
-    let records: Vec<RawWorkoutRecord> = records.into_iter().map(Result::unwrap).collect();
-
-    Ok(records)
-}
-
-fn flatten(sets: &[(String, String)]) -> Vec<String> {
-    sets.iter().fold(vec![], |mut array, tup| {
-        array.push(tup.0.clone());
-        array.push(tup.1.clone());
-        array
-    })
-}
-
-fn build_workout_csv_for_clipboard(records: Vec<RawWorkoutRecord>) {
-    let last_four_days_records = LastFourDaysWorkouts::from(records);
-
-    for data in last_four_days_records.data {
-        println!("Date: {}", &data.0);
-        for exercise_row in data.1.exercises.map {
-            let row = flatten(&exercise_row.1.sets);
-
-            // Just print it out because csv writer throws a wobbly when the rows are different
-            // lengths
-            println!("{}", row.join(","));
+impl App {
+    fn view_file(file: &FileDetails) -> Html {
+        html! {
+            <div class="preview-tile">
+                <b><p class="preview-name">{ format!("{}", file.name) }</p></b>
+                <p class="preview-name" style="white-space: pre-line">{ format!("{}", &file.data) }</p>
+            </div>
         }
-        println!("")
+    }
+
+    fn process_files(files: Option<FileList>) -> Msg {
+        let mut result = Vec::new();
+
+        if let Some(files) = files {
+            let files = js_sys::try_iter(&files)
+                .unwrap()
+                .unwrap()
+                .map(|v| web_sys::File::from(v.unwrap()))
+                .map(File::from);
+            result.extend(files);
+        }
+        Msg::Files(result)
     }
 }
 
-fn deserialize_steps_csv(mut rdr: Reader<File>) -> Result<Vec<StepsRecord>, Box<dyn Error>> {
-    let new_headers = rdr
-        .headers()
-        .iter()
-        .next()
-        .unwrap()
-        .iter()
-        .map(|h| match h {
-            "Type" => "_type",
-            "Exercise Calories" => "exercise_calories",
-            "Exercise Minutes" => "exercise_minutes",
-            "Reps Per Set" => "rps",
-            _ => h,
-        })
-        .map(|h| h.to_lowercase())
-        .collect::<csv::StringRecord>();
-
-    rdr.set_headers(new_headers);
-
-    // Partition suggestion taken from https://doc.rust-lang.org/rust-by-example/error/iter_result.html
-    let (records, _): (Vec<_>, Vec<_>) = rdr.deserialize().partition(Result::is_ok);
-    let records: Vec<StepsRecord> = records.into_iter().map(Result::unwrap).collect();
-
-    Ok(records)
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
-    let filename = args.path;
-    match args.data_type.as_ref() {
-        "nutrition" => {
-            let csv_reader = read_csv(filename)?;
-            let records = deserialize_nutrition_csv(csv_reader).unwrap();
-            build_nutrition_csv_for_clipboard(records);
-        }
-        "weight" => {
-            let csv_reader = read_csv(filename)?;
-            let records = deserialize_weight_csv(csv_reader).unwrap();
-            generic_build_csv_for_clipboard(records);
-        }
-        "steps" => {
-            let csv_reader = read_csv(filename)?;
-            let records = deserialize_steps_csv(csv_reader).unwrap();
-            generic_build_csv_for_clipboard(records);
-        }
-        "workouts" => {
-            let csv_reader = read_csv(filename)?;
-            let records = deserialize_workout_csv(csv_reader).unwrap();
-            build_workout_csv_for_clipboard(records);
-        }
-        _ => (),
-    }
-    Ok(())
+fn main() {
+    yew::Renderer::<App>::new().render();
 }
---
+++ src/mfp.rs
@@ -0,0 +1,255 @@
+// use std::collections::HashMap;
+use indexmap::IndexMap;
+use std::error::Error;
+use std::fmt::Debug;
+use std::io::Read;
+
+use csv::Reader;
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct RawNutritionRecord {
+    date: String,
+    meal: String,
+    calories: String,
+    fat: String,
+    saturated_fat: String,
+    polyunsaturated_fat: String,
+    monounsaturated_fat: String,
+    trans_fat: String,
+    cholesterol: String,
+    sodium: String,
+    potassium: String,
+    carbohydrates: String,
+    fiber: String,
+    sugar: String,
+    protein: String,
+    vitamin_a: String,
+    vitamin_c: String,
+    calcium: String,
+    iron: String,
+    note: Option<String>,
+}
+
+#[derive(Debug, serde::Serialize)]
+struct ProcessedNutritionRecord {
+    protein: f64,
+    carbohydrates: f64,
+    fat: f64,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct WeightRecord {
+    date: String,
+    body_fat: Option<String>,
+    weight: String,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct StepsRecord {
+    date: String,
+    exercise: String,
+    _type: String,
+    exercise_calories: String,
+    exercise_minutes: String,
+    sets: String,
+    rps: String,
+    kilograms: String,
+    steps: String,
+    note: String,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct RawWorkoutRecord {
+    date: String,
+    workout_name: String,
+    duration: String,
+    exercise_name: String,
+    set_order: String,
+    weight: String,
+    reps: String,
+    distance: String,
+    seconds: String,
+    notes: String,
+    workout_notes: String,
+    rpe: String,
+}
+
+#[derive(Debug, serde::Serialize)]
+struct ProcessedWorkoutRecord {
+    workout_name: String,
+    weight: String,
+    reps: String,
+}
+
+fn deserialize_nutrition_csv<R>(
+    mut rdr: csv::Reader<R>,
+) -> Result<Vec<RawNutritionRecord>, Box<dyn Error>>
+where
+    R: std::io::Read,
+{
+    let new_headers = rdr
+        .headers()
+        .iter()
+        .next()
+        .unwrap()
+        .iter()
+        .map(|h| match h {
+            "Fat (g)" => "fat",
+            "Sodium (mg)" => "sodium",
+            "Carbohydrates (g)" => "carbohydrates",
+            "Protein (g)" => "protein",
+            "Saturated Fat" => "saturated_fat",
+            "Polyunsaturated Fat" => "polyunsaturated_fat",
+            "Monounsaturated Fat" => "monounsaturated_fat",
+            "Trans Fat" => "trans_fat",
+            "Vitamin A" => "vitamin_a",
+            "Vitamin C" => "vitamin_c",
+            _ => h,
+        })
+        .map(|h| h.to_lowercase())
+        .collect::<csv::StringRecord>();
+
+    rdr.set_headers(new_headers);
+
+    // Partition suggestion taken from https://doc.rust-lang.org/rust-by-example/error/iter_result.html
+    let (records, _): (Vec<_>, Vec<_>) = rdr.deserialize().partition(Result::is_ok);
+    let records: Vec<RawNutritionRecord> = records.into_iter().map(Result::unwrap).collect();
+
+    Ok(records)
+}
+
+fn deserialize_weight_csv<R>(mut rdr: csv::Reader<R>) -> Vec<String>
+where
+    R: std::io::Read,
+{
+    let new_headers = rdr
+        .headers()
+        .iter()
+        .next()
+        .unwrap()
+        .iter()
+        .map(|h| match h {
+            "Body Fat %" => "body_fat",
+            _ => h,
+        })
+        .map(|h| h.to_lowercase())
+        .collect::<csv::StringRecord>();
+
+    rdr.set_headers(new_headers);
+
+    // Partition suggestion taken from https://doc.rust-lang.org/rust-by-example/error/iter_result.html
+    let (records, _): (Vec<_>, Vec<_>) = rdr.deserialize().partition(Result::is_ok);
+    let records: Vec<WeightRecord> = records.into_iter().map(Result::unwrap).collect();
+
+    let mut only_weight = vec![];
+    for record in records {
+        only_weight.push(record.weight);
+    }
+
+    only_weight
+}
+
+fn build_nutrition_csv_for_clipboard<'a>(
+    records: Vec<RawNutritionRecord>,
+    buf: Box<dyn std::io::Write + 'a>,
+) {
+    let mut wtr = csv::Writer::from_writer(buf);
+    let mut records_grouped_by_date: IndexMap<String, ProcessedNutritionRecord> = IndexMap::new();
+
+    for record in records {
+        records_grouped_by_date
+            .entry(record.date)
+            .and_modify(|pr| {
+                pr.protein += record.protein.parse::<f64>().unwrap();
+                pr.carbohydrates += record.carbohydrates.parse::<f64>().unwrap();
+                pr.fat += record.fat.parse::<f64>().unwrap();
+            })
+            .or_insert(ProcessedNutritionRecord {
+                protein: record.protein.parse::<f64>().unwrap(),
+                carbohydrates: record.carbohydrates.parse::<f64>().unwrap(),
+                fat: record.fat.parse::<f64>().unwrap(),
+            });
+    }
+    let processed_records = records_grouped_by_date.values().collect::<Vec<_>>();
+
+    for record in processed_records {
+        wtr.serialize(record).ok();
+    }
+    wtr.into_inner().ok();
+}
+
+fn generic_build_csv_for_clipboard<'a, R>(records: Vec<R>, buf: Box<dyn std::io::Write + 'a>)
+where
+    R: serde::Serialize,
+{
+    let mut wtr = csv::Writer::from_writer(buf);
+
+    for record in records {
+        wtr.serialize(record).ok();
+    }
+    wtr.into_inner().ok();
+}
+
+fn deserialize_steps_csv<B>(mut rdr: Reader<B>) -> Vec<String>
+where
+    B: std::io::Read,
+{
+    let new_headers = rdr
+        .headers()
+        .iter()
+        .next()
+        .unwrap()
+        .iter()
+        .map(|h| match h {
+            "Type" => "_type",
+            "Exercise Calories" => "exercise_calories",
+            "Exercise Minutes" => "exercise_minutes",
+            "Reps Per Set" => "rps",
+            _ => h,
+        })
+        .map(|h| h.to_lowercase())
+        .collect::<csv::StringRecord>();
+
+    rdr.set_headers(new_headers);
+
+    // Partition suggestion taken from https://doc.rust-lang.org/rust-by-example/error/iter_result.html
+    let (records, _): (Vec<_>, Vec<_>) = rdr.deserialize().partition(Result::is_ok);
+    let records: Vec<StepsRecord> = records.into_iter().map(Result::unwrap).collect();
+
+    let mut only_steps = vec![];
+    for record in records {
+        only_steps.push(record.steps);
+    }
+
+    only_steps
+}
+
+pub fn process<B>(file_bytes: B, file_type: &str) -> String
+where
+    B: std::io::Read,
+{
+    let csv_reader = csv::Reader::from_reader(file_bytes);
+    let mut buf = vec![];
+
+    match file_type {
+        "nutrition" => {
+            let records = deserialize_nutrition_csv(csv_reader).unwrap();
+            build_nutrition_csv_for_clipboard(records, Box::new(&mut buf));
+        }
+        "weight" => {
+            let records = deserialize_weight_csv(csv_reader);
+            generic_build_csv_for_clipboard(records, Box::new(&mut buf));
+        }
+        "steps" => {
+            let records = deserialize_steps_csv(csv_reader);
+            generic_build_csv_for_clipboard(records, Box::new(&mut buf));
+        }
+        _ => (),
+    };
+
+    //from https://stackoverflow.com/questions/63024483/writing-to-a-file-or-string-in-rust
+    let mut bytes = &buf[..];
+    let mut out = String::new();
+    bytes.read_to_string(&mut out).unwrap();
+    out
+}
